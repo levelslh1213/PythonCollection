9e4fada23995c5725f0ebcb3969c194796363c83
paulo.paes <paulo.paes@edata.com.br>
2022-08-15 19:57:57 -0300

    Understandind sintaxe of python and functions

---
+++ Introducao_Python/dia1.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from ast import And, Not, Or
+
+
+print("Ola Mundo")
+print("Primeira mensagem")
+#comentando algo
+
+"""
+comentario de muitas linhas
+"""
+
+######### operações matemáticas ##############
+print(2+2)
+print(2-5)
+print(2**2) #exponenciação
+print(10/3)
+print(10%3) #resto da divisão
+
+######### Declarações de variáveis ##############
+"""
+Python é uma linguagem de tipagem fraca então as veriáveis são declaradas na sua atribuição de valor
+"""
+x = 5
+y = 5
+print(x**y)
+z = True
+print(z)
+
+######### Operadores ##############
+
+# Relacionais
+x > y
+x < y
+x = y
+x == y
+x != y
+x >= y
+x <= y
+
+# Lógicos
+
+And
+Or
+Not
+
+#shift + enter executar python
---
+++ Introducao_Python/teste_function.py
@@ -0,0 +1,4 @@
+class Dog:
+    def __init__(self, raca, idade):
+        self.raca = raca
+        self.idade = idade
